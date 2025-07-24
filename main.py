a251ae1b59dd15f3a5058e1d70bb6baa0f859fb3
Josh Burne <joshb.contact@gmail.com>
2025-07-24 10:19:26 +0100

    rebuilding success, CH1 L4, inputs and outputs with CLI sys arg

--- main.py
+++ main.py
@@ -1,31 +1,23 @@
 import os
-import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-if len(sys.argv) >= 2:
-
-    response = client.models.generate_content(
-        model='gemini-2.0-flash-001',
-        contents= sys.argv[1]
-        
-    )
+if len(sys.argv) > 1:
+    arg_response = sys.argv[1]
 else:
-    raise Exception ("No prompt given")
+    print("Error: Please run the code with an argument")
     sys.exit(1)
+    
 
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', 
+    contents=arg_response)
 print(response.text)
-
-if len(sys.argv) >= 3:
-    if sys.argv[2] == "--verbose":
-        print(f"User prompt: {sys.argv[1]}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-
-
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
