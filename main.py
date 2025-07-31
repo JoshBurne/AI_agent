c8c7087abbe1d3bdef97d54cae665a935aaa6e48
Josh Burne <joshb.contact@gmail.com>
2025-07-31 17:39:59 +0100

    finished AI Agent project

--- main.py
+++ main.py
@@ -8,69 +8,91 @@ import sys
 
 def main ():
     load_dotenv()
+
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+    
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I fix the calculator?"')
+        sys.exit(1)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    user_prompt = " ".join(args)
 
-
-    # PROMPT IS CHOSEN FROM THE SECOND CLI ARGUMENT AFTER THE SCRIPT NAME
-    if len(sys.argv) > 1:
-        arg_response = sys.argv[1]
-    else:
-        # exit with error code if not second cli arg is given.
-        print("Error: Please run the code with an argument")
-        sys.exit(1)
-        
-
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+    
     messages = [
-        types.Content(role="user", parts=[types.Part(text=arg_response)]),
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
 
-    #generate a response using google gemini:
+
+    iters = 0
+    while True:
+        iters +=1
+        if iters > 20:
+            print(f"maximum iterations reached.")
+            sys.exit(1)
+
+        try:
+            final_response = generate_content(client, messages, verbose)
+            if final_response:
+                print("Final response:")
+                print(final_response)
+                break
+        except Exception as e:
+            print(f"Error in generate_content: {e}")
+            return
+
+def generate_content(client, messages, verbose):
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
         contents=messages,
-            config=types.GenerateContentConfig(
-                tools=[available_functions], system_instruction=system_prompt
-            ),
-        )
-    if len(sys.argv) >= 3:
-        if sys.argv[2] == "--verbose":
-            # print the users prompt
-            print(f"User prompt: {arg_response}")
-            # print the tokens used in the prompt
-            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-            # print the tokens used in the response
-            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-        else: 
-            raise Exception ("sys.argv[2] should only be 'None' or '--verbose'")
-        
+        config=types.GenerateContentConfig(
+            tools=[available_functions], 
+            system_instruction=system_prompt
+        ),
+    )
+
+    if verbose:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    if response.candidates:
+        for candidate in response.candidates:
+            function_call_content = candidate.content
+            messages.append(function_call_content)
 
     if not response.function_calls:
-        print (response.text)
-        sys.exit(0)
-
+        return response.text
+    
+    function_responses = []
     for function_call_part in response.function_calls:
-        # call the functuion and get the result
-        function_result = call_function(function_call_part, verbose="--verbose" in sys.argv)
-
-        if not function_result.parts or not function_result.parts[0].function_response or not function_result.parts[0].function_response.response:
-            # Did the function have no parts at all? If yes, thats bad
-            # OR
-            # Did the first part exist, but it doesn't contain the function's response. Still bad.
-            # OR
-            # The function ran, but it didn’t return any result inside the expected structure. Still bad.
-            raise Exception ("Fatal error, function did not return a valid response")
+        function_call_result = call_function(function_call_part,verbose)
+        if (
+            not function_call_result.parts
+            or not function_call_result.parts[0].function_response
         
-        response_content = function_result.parts[0].function_response.response.get("result")
-        if "--verbose" in sys.argv:
-            print(f"-> {response_content}")
-        else:
-            print(response_content)
-        
+        ):
+            raise Exception ("empty function call result")
+        if verbose:
+            print(f"-> {function_call_result.parts[0].function_response.response}")
+        function_responses.append(function_call_result.parts[0])
 
+    if not function_responses:
+        raise Exception("no function responses generated, exiting.")
+    
+    messages.append(types.Content(role="tool", parts=function_responses))
 
-
+    
 
 
 if __name__ == "__main__":
